cefe3279e0334186f7a233bf10128c2991aac727
nathanolah <nathan.olah@protonmail.com>
2022-03-10 12:13:51 -0500

    updates on web

--- readit-web/src/pages/register.tsx
+++ readit-web/src/pages/register.tsx
@@ -1,8 +1,46 @@
 import React from "react";
+import { Formik, Form } from 'formik';
+import { FormControl, FormLabel, Input, FormErrorMessage, Box, Button } from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
 
 const Register = () => {
     return (
-        <div>register page</div>
+        <Wrapper variant='small'>
+            <Formik
+                initialValues={{ username: "", password: ""}}
+                onSubmit={(values) => {
+                    console.log(values)
+                }}
+            >
+                {({ values, handleChange, isSubmitting }) => (
+                    <Form>
+                        <InputField
+                            name="username"
+                            placeholder="username"
+                            label="Username"
+                        />
+                        <Box mt={4}>
+                            <InputField
+                                name="password"
+                                placeholder="password"
+                                label="Password"
+                                type="password"
+                            />
+                        </Box>
+                        <Box mt={4}>
+                            <Button 
+                                type='submit' 
+                                colorScheme='teal'
+                                isLoading={isSubmitting}
+                            >
+                                Register
+                            </Button>
+                        </Box>
+                    </Form>
+                )}
+            </Formik>
+        </Wrapper>
     );
 }
 
